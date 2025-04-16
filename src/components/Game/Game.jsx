80d8be1d358e023498bd0165c7ca1c5eb1a6bb0b
Desair1 <ilya_velichko_1995@mail.ru>
2025-04-16 19:06:34 +0300

    updated state for InfoLayout

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,18 +4,22 @@ import Information from "../Information/Information";
 import styles from "./Game.module.css";
 
 const Game = () => {
-	const [currentPlayer, setCurrentPlayer] = useState("X");
-	const [isGameEnded, setIsGameEnded] = useState(false);
-	const [isDraw, setIsDraw] = useState(false);
-	const [field, setField] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [isGameEnded, setIsGameEnded] = useState(false);
+  const [isDraw, setIsDraw] = useState(false);
+  const [field, setField] = useState(["", "", "", "", "", "", "", "", ""]);
 
-	return (
-		<main className={styles.container}>
-			<Information isGameEnded={isGameEnded} isDraw={isDraw} />
-			<Field field={field} />
-			<button>Начать сначала</button>
-		</main>
-	);
+  return (
+    <main className={styles.container}>
+      <Information
+        currentPlayer={currentPlayer}
+        isGameEnded={isGameEnded}
+        isDraw={isDraw}
+      />
+      <Field field={field} />
+      <button>Начать сначала</button>
+    </main>
+  );
 };
 
 export default Game;
--- src/components/Information/InformationLayout.jsx
+++ src/components/Information/InformationLayout.jsx
@@ -1,16 +1,19 @@
 import styles from "./Information.module.css";
 
-const InformationLayout = ({ isGameEnded, isDraw }) => {
-	return (
-		<div className={styles["header-info"]}>
-			<h1>Крестики-нолики</h1>
-			<h2>Текущий счёт: </h2>
-			<h3>Текущий ход: </h3>
+const InformationLayout = ({ currentPlayer, isGameEnded, isDraw }) => {
+  return (
+    <div className={styles["header-info"]}>
+      <h1>Крестики-нолики</h1>
+      <h2>Текущий счёт: </h2>
+      <h3>
+        Текущий ход:
+        {isGameEnded && isDraw === false ? `Ходит: ${currentPlayer}` : ""}{" "}
+      </h3>
 
-			{isGameEnded ? <div>Игра окончена</div> : ""}
-			{isDraw ? <div>Ничья!</div> : ""}
-		</div>
-	);
+      {isGameEnded ? `Победа ${currentPlayer}` : ""}
+      {isDraw ? <div>Ничья!</div> : ""}
+    </div>
+  );
 };
 
 export default InformationLayout;
